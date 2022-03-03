da7f5f2462cf77c947f533f4313913ab4d424e68
LuisJoseTrujiloP <luisjosetrujillodeveloper@gmail.com>
2022-03-03 14:19:44 -0500

    Finish Promises with API

--- src/promise/challenge.js
+++ src/promise/challenge.js
@@ -1,5 +1,5 @@
-const fetchData = require ('../utils/fetchData');
-const API = 'https://rickandmortyapi.com/api.character/';
+const fetchData = require ('../utils/fetchData.js');
+const API = 'https://rickandmortyapi.com/api/character/';
 
 
 
@@ -10,13 +10,13 @@ fetchData(API)
         return fetchData(`${API}${data.results[0].id}`)
     })
     .then(data => {
-        console.log(data.name)
+        console.log(data.name);
         return fetchData(data.origin.url)
     })
     .then(data => {
         console.log(data.dimension)
     })
-    .catch (err => console.error(err))
+    .catch (err => console.error(err));
 
 
 
